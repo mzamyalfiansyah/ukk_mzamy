179146e91b107e1ad81763d5c5957b8c7aa886b4
mzamyalfiansyah <111264565+mzamyalfiansyah@users.noreply.github.com>
2024-03-20 23:19:51 +0700

    hps produk

--- resources/views/data_produk.blade.php
+++ resources/views/data_produk.blade.php
@@ -278,34 +278,18 @@
                       {{-- HAPUS --}}
                     
                       <td>
-                        <button type="submit" class="btn btn-danger btn-sm"  data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        <a class="btn btn-danger remove" href="/proses_delete/{{$produk->produk_id}}">
                         <!-- <a  class="btn btn-danger remove" href="/proses_delete/{{$produk->produk_id}}" ></a> -->
                         <!-- <input type="hidden" value="{{$produk->produk_id}}"> -->
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
                             </svg>
-  </button>
-                        
-                        {{-- KONFIRMASI HAPUS --}}
-                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                          <div class="modal-dialog">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Yakin ingin menghapus?</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                              </div>
-                              <div class="modal-body">
-                                ini mungkin tidak dapat dikembalikan!!
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Batal</button>
+                        </a>
+      
                                 
-                                <a  class="btn btn-danger remove" href="/proses_delete/{{$produk->produk_id}}" >hapus</a>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                                <a   >hapus</a>
+                              
 
 
                         {{-- DETAIL --}}
